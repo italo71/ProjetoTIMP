e04151e852cdaf8fbe1afeb4fa43879ab2b78580
Clousbi <biancamthope@gmail.com>
2023-03-10 09:55:55 -0300

    Session storage

--- ProjetoTIMP/src/app/pages/pre-login/cadastro/cadastro.component.ts
+++ ProjetoTIMP/src/app/pages/pre-login/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
