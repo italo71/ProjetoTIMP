9916de1edfed40272d48300de1e265afbcacefeb
italo71 <102165012+italo71@users.noreply.github.com>
2023-03-29 09:33:24 -0300

    Rota de tarefas

--- ProjetoTIMP/src/app/pages/menu/tarefas/tarefas.component.ts
+++ ProjetoTIMP/src/app/pages/menu/tarefas/tarefas.component.ts
@@ -11,21 +11,34 @@ export class TarefasComponent implements OnInit {
   sidebarExpanded = true;
   titulo: string = '';
   descricao: string = '';
+  tarefas: Array<any> = [];
   constructor(
     private rest: TarefasService,
     private comu: CominucacaoService,
   ) { }
 
   ngOnInit(): void {
-    this.rest.obterTarefas().subscribe((data: any) => {
-      if (data.status == "success") {
-        console.log(data.data)
-      }
+    this.getTarefas();
+  }
 
-      else {
-        console.log(data.message)
+  getTarefas() {
+    this.rest.obterTarefas().subscribe((data: any) => {
+      console.log(data)
+      if (data.status == "success") {
+        if (data.data.length != 0) {
+          this.tarefas = []
+          data.data.forEach((data: any) => {
+            this.tarefas.push({ "titulo": data.titulo, "descricao": data.descricao, "data_criacao": data.data_criacao })
+          });
+        }
       }
     })
+  }
+
+  abrirModal(titulo: string, descricao: string) {
+    console.log(titulo + '  ' + descricao);
+    $('#staticBackdropLabel').text(titulo);
+    $('#descricao_modal').text(descricao);
   }
 
   addTarefa() {/* .replace(/ /g, '') */
@@ -35,7 +48,14 @@ export class TarefasComponent implements OnInit {
         if (data.status == 'success') {
           this.titulo = '';
           this.descricao = '';
-          $('#message').val(data.message)
+          $('#message').text(data.message)
+          let timer = setTimeout(function () {
+            if ($('#message').text() != '')
+              $('#message').text('')
+            else
+            clearInterval(timer)
+          }, 5000);
+          this.getTarefas()
         }
       })
     }
