80ef3d872452cbc7130be1702b461b70ce1b0e38
Italo71 <102165012+italo71@users.noreply.github.com>
2023-05-10 19:54:54 -0300

    mensagem de erro no login

--- ProjetoTIMP/src/app/pages/pre-login/login/login.component.ts
+++ ProjetoTIMP/src/app/pages/pre-login/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
     login: [null, [Validators.required]],
     senha: [null, [Validators.required]]
   });
+  msg:string = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,11 +34,15 @@ export class LoginComponent implements OnInit {
     let login = this.resourceForm.get('login')?.value
     let senha = this.resourceForm.get('senha')?.value
     this.comu.filter('loading');
+    this.msg = '';
     this.session.logar(login, senha).subscribe((data: any) => {
       if (data.status == 'success') {
         this.session.criarSessao(data.data);
         this.comu.filter('loaded');
         this.router.navigate(['/menu']);
+      }
+      else{
+        this.msg = data.message
       }
       this.comu.filter('loaded');
     })
