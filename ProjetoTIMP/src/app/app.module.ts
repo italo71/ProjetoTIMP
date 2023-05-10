4248441e2112bf61ed426f037cf47f70080538c9
Clousbi <biancamthope@gmail.com>
2023-05-10 09:52:32 -0300

    agenda - atualização

--- ProjetoTIMP/src/app/app.module.ts
+++ ProjetoTIMP/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -33,6 +33,7 @@ import { InterceptorService } from './shared/interceptor.service';
       useClass: InterceptorService,
       multi: true
     },
+    
   ],
   bootstrap: [AppComponent]
 })
--- ProjetoTIMP/src/app/pages/menu/menu.module.ts
+++ ProjetoTIMP/src/app/pages/menu/menu.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,6 +11,14 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { TarefasComponent } from './tarefas/tarefas.component';
 
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CalendarModule, DateAdapter, MOMENT } from 'angular-calendar';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+
+registerLocaleData(localeIt);
+import * as moment from 'moment';
 
 @NgModule({
   declarations: [
@@ -26,14 +34,23 @@ import { SharedModule } from 'src/app/shared/shared.module';
     SharedModule,
     RouterModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    SchedulerModule.forRoot({ locale: 'pt', headerDateFormat: 'daysRange' })
   ],
-  exports:[
+  exports: [
     MenuComponent,
     AgendaComponent,
     MetasComponent,
     PerfilComponent,
     TarefasComponent
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MOMENT, useValue: moment }
   ]
 })
 export class MenuModule { }
