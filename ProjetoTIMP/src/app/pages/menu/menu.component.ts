cc30012e194dd13be8013cdc0da2f03f1b0b4b24
Italo71 <102165012+italo71@users.noreply.github.com>
2023-06-13 10:53:02 -0300

    Finalização menu

--- ProjetoTIMP/src/app/pages/menu/menu.component.ts
+++ ProjetoTIMP/src/app/pages/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AgendaService } from 'src/app/shared/service/agenda.service';
 import { CominucacaoService } from 'src/app/shared/service/cominucacao.service';
 import { SessionService } from 'src/app/shared/service/session.service';
 
@@ -12,14 +13,48 @@ export class MenuComponent implements OnInit {
   dataNasc = this.formatarData(this.session.obterDataNascUsu());
   nomeUsuario = this.session.obterNomeUsu();
   emailUsuario = this.session.obterEmailUsu();
+  data: any = new Date().toLocaleDateString();
+  listaAgenda: Array<any> = [];
+  listaExibir: Array<any> = [];
   constructor(
     private session: SessionService,
-    private comu: CominucacaoService
+    private comu: CominucacaoService,
+    private agendaRest: AgendaService
   ) { }
 
   ngOnInit(): void {
     this.comu.filter('loaded');
     this.formatarData(this.dataNasc);
+    this.listaAgenda = [];
+    if (this.agendaRest.getAgendaStorage() != null) {
+      this.listaAgenda = this.agendaRest.getAgendaStorage();
+      this.obterEventos();
+    }
+    else {
+      let aux = this.listaAgenda;
+      this.agendaRest.obterAgenda().subscribe((data: any) => {
+        if (data.status == 'success') {
+          let dados: Array<any> = data.data;
+          this.listaAgenda = dados;
+          this.agendaRest.agendaStorage(this.listaAgenda);
+        }
+        else {
+          this.listaAgenda = aux;
+        }
+        if (this.listaAgenda.length > 0) {
+          this.obterEventos();
+        }
+      });
+    }
+  }
+
+  obterEventos() {
+    console.log(this.listaAgenda);
+    for (let i = 0; i < this.listaAgenda.length; i++) {
+      if (new Date(this.listaAgenda[i].data_inicio).toISOString() >= new Date().toISOString()) {
+        this.listaExibir.push(this.listaAgenda[i]);
+      }
+    }
   }
 
   formatarData(data: any) {
@@ -33,5 +68,9 @@ export class MenuComponent implements OnInit {
     }
     r = r.slice(0, -1)
     return r
+  }
+
+  stringData(v: any) {
+    return new Date(v).toLocaleDateString();
   }
 }
